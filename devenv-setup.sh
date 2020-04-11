6e7a23af5e7e6e84f0de882744a5f68fd8f6b4fd
andrewnm9 <32853769+andrewnm9@users.noreply.github.com>
2020-04-11 11:24:07 -0400

    initial rails app running and shell script created with setup steps

--- devenv-setup.sh
+++ devenv-setup.sh
@@ -4,15 +4,17 @@ dbuser="$1"
 dbpass="$2"
 appname="stocks"
 
-sudo apt install curl
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install curl -y
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn -y
 
 sudo apt-get update
-sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev -y
 
-sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev -y
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
@@ -22,14 +24,20 @@ ruby -v
 gem install bundler
 gem install rails -v 6.0.2.2
 rails -v
-sudo apt install postgresql-11 libpq-dev
+sudo apt-get install postgresql postgresql-contrib libpq-dev -y
+sudo service postgresql start
 sudo -u postgres createuser $dbuser -s
 sudo -u postgres psql
-postgres=# \password $dbpass
+### Run these commands to change password in postgres
+#\password $dbuser
+#$dbpass
+#$dbpass
+#\q
 
-rails new $appname -d postgresql
+rails new $appname -d postgresql --skip-spring --skip-listen --force
 cd $appname
-sed -i -e "s/username:.*/username: $dbuser" -e "s/password:.*/password: $dbpass" config/database.yml
+sed -i -e "s/username:.*/username: $dbuser/" -e "s/password:.*/password: $dbpass/" config/database.yml
 rake db:create
+sed -i -e "s/check_yarn_integrity: true/check_yarn_integrity: false/" config/webpacker.yml
 echo "$appname app is being started on http://localhost:3000"
 rails server
